4d2c5ed4819b55952659dd9bb8e9ead11d624cb4
Tucker <borisblade17@gmail.com>
2018-07-14 19:00:50 -0500

    5-saveNewHome - when a home is successfully created redirect to home edit screen - Tucker

--- src/Components/HomeEntry.js
+++ src/Components/HomeEntry.js
@@ -1,6 +1,7 @@
 ﻿import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom'
 
-import HomeService from '../Services/HomeService'
+import HomeService from '../Services/HomeService';
 import PropTypes from 'prop-types';
 import { withStyles } from 'material-ui/styles';
 import Button from 'material-ui/Button';
@@ -56,7 +57,10 @@ class HomeEntry extends Component {
   state = {
     name: '',
     occupants: '',
-    hasError: false
+    homeId: '',
+    hasError: false,
+    redirect: false,
+    redirectUrl: 'homeEdit/test',
   };
 
   componentDidMount() {
@@ -71,29 +75,39 @@ class HomeEntry extends Component {
 
   postHome() {
     HomeService.postHome(this.state.name, this.state.occupants)
-        .then((response) => {
-            response.json()
-            if(response.status == 200) {
-              this.setState({
-                hasError: false,
-              });
-            } else {
-              this.setState({
-                hasError: true,
-              });
-            }   
-        })
-        .then((responseJson) => {
-            if (!this.state.hasError) {
-              this.setState({
-                name: responseJson.name,
-                occupants: responseJson.occupants,
-              });
-            }
-        })
+        .then((response) => this.handleError(response))
+        .then((response) => response.json())
+        .then((responseJson) => this.handleResponse(responseJson))
         .catch((error) => {
-          console.log('message');
+          console.log(error);
         });
+  }
+
+  handleError(response) {
+    if(response.status == 200) {
+      this.setState({
+        homeId: this.state.name,
+        hasError: false,
+      });
+    } else {
+      this.setState({
+        hasError: true,
+        redirect: false,
+      });
+    }
+    return response;
+  }
+
+  handleResponse(responseJson) {
+    if (!this.state.hasError) {
+      this.setState({
+        homeId: responseJson._id,
+        name: responseJson.name,
+        occupants: responseJson.occupants,
+        redirect: true,
+        redirectUrl: "homeEdit/" + responseJson._id
+      });
+    }
   }
 
   render() {
@@ -102,12 +116,13 @@ class HomeEntry extends Component {
     return (
         <div className="HomeEntry">
             <Paper className={classes.root}>
+                {this.state.redirect ? <Redirect to={this.state.redirectUrl} /> : null}
                 {this.state.hasError ? <ErrorMessage /> : null}
                 <Typography variant="headline" component="h3">
                     Enter New Home
                 </Typography>
                 <Grid container>
-                    <Grid item sm={0} md={3}/>
+                    <Grid item md={3}/>
                     <Grid item xs={12} sm={6} md={3}>
                         <TextField
                             id="home-name"
@@ -128,22 +143,22 @@ class HomeEntry extends Component {
                             onChange={(event) => this.setState({occupants: event.target.value})}
                         />            
                     </Grid>
-                    <Grid item sm={0} md={3}/>
+                    <Grid item md={3}/>
                 </Grid>
             </Paper>
             <Grid container>
-                <Grid item sm={0} sm={2} md={3}/>
+                <Grid item sm={2} md={3}/>
                 <Grid item xs={12} sm={4} md={3}>
-                    <Button variant="raised" cxdsw color="secondary" className={classes.saveButton} onClick={this.postHome.bind(this)}>
+                    <Button variant="raised" color="secondary" className={classes.saveButton} onClick={this.postHome.bind(this)}>
                         Save Home
                     </Button>
                 </Grid>
                 <Grid item xs={12} sm={4} md={3}>
-                    <Button variant="raised" cxdsw href="/mainMenu" color="secondary" className={classes.cancelButton}>
+                    <Button variant="raised" href="/mainMenu" color="secondary" className={classes.cancelButton}>
                         Cancel
                     </Button>
                 </Grid>
-                <Grid item sm={0} sm={2} md={3}/>
+                <Grid item sm={2} md={3}/>
             </Grid>
         </div>
       );
--- src/Routes.js
+++ src/Routes.js
@@ -3,13 +3,14 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import MainMenu from './Components/MainMenu';
 import HomeEntry from './Components/HomeEntry';
-//import Cart from './Components/Cart'
+import HomeEdit from './Components/HomeEdit';
 
 export default () => (
   <BrowserRouter>
     <Switch>
       <Route path="/mainMenu" exact component={MainMenu} />
       <Route path="/homeEntry" exact component={HomeEntry} />
+      <Route path="/homeEdit/:homeId" exact component={HomeEdit} />
     </Switch>
   </BrowserRouter>
 );
