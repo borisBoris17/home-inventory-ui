c52aa2912aa42f5d1cc61be49bbb7cbd4421dc20
Tucker <tucker.bichsel@rgare.com>
2018-11-06 17:01:04 -0600

    11 - update home Edit screen to show a table of Rooms - Tucker

--- src/Components/Home/HomeEdit.js
+++ src/Components/Home/HomeEdit.js
@@ -1,13 +1,15 @@
-﻿import React, { Component } from 'react';
+﻿import React, {Component} from 'react';
 
 import HomeService from '../../Services/HomeService';
 import PropTypes from 'prop-types';
-import { withStyles } from 'material-ui/styles';
+import {withStyles} from 'material-ui/styles';
 import Paper from 'material-ui/Paper';
 import Typography from 'material-ui/Typography';
 import Card from 'material-ui/Card';
 import Grid from "material-ui/Grid/Grid";
 import Button from "material-ui/Button/Button";
+import {TableBody, TableCell, TableHead, TableRow} from "material-ui";
+import Table from "material-ui/Table/Table";
 
 const styles = theme => ({
     root: theme.mixins.gutters({
@@ -20,16 +22,19 @@ const styles = theme => ({
     }),
     homeCard: {
         marginRight: 8,
-        textAlign: 'left',
+        textAlign: 'left'
     },
-    homeCardTitle: {
+    homeLabel: {
+        fontSize: 16,
+        margin: theme.spacing.unit
+    },
+    homeValue: {
         fontSize: 24,
-        borderBottomWidth: 1,
-        borderBottomColor: 'grey',
-        borderBottomStyle: 'solid'
+        margin: theme.spacing.unit
     },
     roomsCard: {
         marginLeft: 8,
+        textAlign: 'left'
     },
     textField: {
         marginLeft: theme.spacing.unit,
@@ -64,12 +69,13 @@ const styles = theme => ({
 
 class HomeEdit extends Component {
     state = {
-      home: {
-          id: this.props.match.params.homeId,
-          name: '',
-          occupants: ''
-      },
-      hasError: false
+        home: {
+            id: this.props.match.params.homeId,
+            name: '',
+            occupants: '',
+            rooms: []
+        },
+        hasError: false
     };
 
     componentDidMount() {
@@ -83,7 +89,7 @@ class HomeEdit extends Component {
     }
 
     handleError(response) {
-        if(response.status == 200) {
+        if (response.status == 200) {
             this.setState({
                 home: {
                     id: this.state.name
@@ -105,57 +111,119 @@ class HomeEdit extends Component {
                     id: responseJson._id,
                     name: responseJson.name,
                     occupants: responseJson.occupants,
+                    rooms: [
+                        {
+                            name: 'Kitchen',
+                            items: []
+                        },
+                        {
+                            name: 'Living Room',
+                            items: [
+                                {
+                                    nonsense: 'nonsense'
+                                }
+                            ]
+                        }
+                    ]
                 }
             });
         }
     }
-    
-    render() {
-      const { classes } = this.props;
 
-      return (
-        <div>
-            <Paper className={classes.root}>
+    render() {
+        const {classes} = this.props;
+
+        return (
+            <div>
+                <Paper className={classes.root}>
+                    <Typography variant="headline" component="h3">
+                        Edit Home
+                    </Typography>
+                    <Grid container>
+                        <Grid item xs={12} sm={4} md={3}>
+                            <div className={classes.homeCard}>
+                                <Typography className={classes.homeLabel} color="secondary">
+                                    Name
+                                </Typography>
+                                <Typography className={classes.homeValue}>
+                                    {this.state.home.name}
+                                </Typography>
+                                <Typography className={classes.homeLabel} color="secondary">
+                                    Occupants
+                                </Typography>
+                                <Typography className={classes.homeValue}>
+                                    {this.state.home.occupants}
+                                </Typography>
+                            </div>
+                        </Grid>
+                        <Grid item xs={12} sm={8} md={9}>
+                            <div className={classes.roomsCard}>
+                                <Typography  className={classes.homeLabel} color="secondary">
+                                    Rooms
+                                </Typography>
+                                {this.state.home.rooms != undefined ? <Table className={classes.table}>
+                                    <TableHead>
+                                        <TableRow>
+                                            <TableCell padding="checkbox" className={classes.tableHeaderCell}>
+                                                Name
+                                            </TableCell>
+                                            <TableCell padding="checkbox" className={classes.tableHeaderCell}>
+                                                Number of Items
+                                            </TableCell>
+                                            <TableCell className={classes.buttonColumn}/>
+                                        </TableRow>
+                                    </TableHead>
+                                    <TableBody>
+                                        {this.state.home.rooms.map(n => {
+                                            return (
+                                                <TableRow className={classes.table} key={n._id}>
+                                                    <TableCell className={classes.tableCell}>{n.name}</TableCell>
+                                                    <TableCell
+                                                        className={classes.tableCell}>{n.items.length}</TableCell>
+                                                    <TableCell className={classes.tableCell}>
+                                                        <Button variant="raised" color="secondary"
+                                                                className={classes.selectButton}
+                                                                onClick={(e) => this.selectHome(n._id)}>
+                                                            Select
+                                                        </Button>
+                                                    </TableCell>
+                                                </TableRow>
+                                            );
+                                        })}
+                                    </TableBody>
+                                </Table> : null}
+                                <Grid container>
+                                    <Grid item sm={2} md={3}/>
+                                    <Grid item xs={12} sm={4} md={3}>
+                                        <Button variant="raised" color="secondary" className={classes.saveButton}>
+                                            Add Room
+                                        </Button>
+                                    </Grid>
+                                    <Grid item xs={12} sm={4} md={3}>
+                                        <Button variant="raised" href="/mainMenu" color="secondary"
+                                                className={classes.cancelButton}>
+                                            Return Home
+                                        </Button>
+                                    </Grid>
+                                    <Grid item sm={2} md={3}/>
+                                </Grid>
+                            </div>
+                        </Grid>
+                    </Grid>
+                </Paper>
                 <Grid container>
-                    <Grid item xs={12} sm={4} md={3}>
-                        <Card className={classes.homeCard}>
-                            <Typography className={classes.homeCardTitle} color="secondary" >
-                                Home
-                            </Typography>
-                            <Typography>
-                                Name
-                            </Typography>
-                            <Typography>
-                                {this.state.home.name}
-                            </Typography>
-                            <Typography>
-                                Occupants
-                            </Typography>
-                            <Typography>
-                                {this.state.home.occupants}
-                            </Typography>
-                        </Card>
-                    </Grid>
-                    <Grid item xs={12} sm={8} md={9}>
-                        <Card className={classes.roomsCard}>
-                            <Typography>
-                                Name
-                            </Typography>
-                            <Typography>
-                                {this.state.home.name}
-                            </Typography>
-                            <Typography>
-                                Occupants
-                            </Typography>
-                            <Typography>
-                                {this.state.home.occupants}
-                            </Typography>
-                        </Card>
-                    </Grid>
+                <Grid item sm={4} md={4}/>
+                <Grid item xs={12} sm={4} md={4}>
+                    <Button variant="raised" href="/mainMenu" color="secondary"
+                            className={classes.cancelButton}>
+                        Return Home
+                    </Button>
                 </Grid>
-            </Paper>
-        </div>
-      );
+                <Grid item sm={4} md={4}/>
+            </Grid>
+            </div>
+        );
     }
-} 
+}
+
 export default withStyles(styles)(HomeEdit);
